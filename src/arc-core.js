cd5a3779fceb7dc9014cedfe6d251ddf65c0f5ee
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-11 19:18:39 -0400

    generated renderer class file trees

--- src/arc-core.js
+++ src/arc-core.js
@@ -1,1 +1,4 @@
-export * from './math/index';
+import * as Renderer from './renderers/index';
+import * as Math from './math/index';
+
+export { Renderer, Math };
---
+++ src/renderers/index.js
@@ -0,0 +1,3 @@
+export { default as Canvas2D } from './canvas-2d/canvas-2d';
+export { default as WebGL } from './webgl/webgl';
+export { default as WebGL2 } from './webgl-2/webgl-2';
