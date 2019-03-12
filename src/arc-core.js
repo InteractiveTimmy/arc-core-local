a5dad7ad52de3760b43c72674c801683df47b3e2
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-11 20:48:09 -0400

    generated uuid function and performance test example

--- src/arc-core.js
+++ src/arc-core.js
@@ -1,5 +1,11 @@
 import * as renderers from './renderers/index';
 import * as math from './math/index';
 import * as core from './core/index';
+import * as utils from './utils/index';
 
-export { renderers, math, core };
+export {
+  renderers,
+  math,
+  core,
+  utils,
+};
