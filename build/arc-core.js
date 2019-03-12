e4b5413a10c1535b4643a6314084eebeee2b357f
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-11 20:48:32 -0400

    updated build

--- build/arc-core.js
+++ build/arc-core.js
@@ -4,7 +4,23 @@
 	(global = global || self, factory(global.ARCCORE = {}));
 }(this, function (exports) { 'use strict';
 
-	var canvas2d = {};
+	class Canvas2D {
+	  constructor() {
+	    this.elm = document.createElement('canvas');
+	    this.ctx = this.element.getContext('2d');
+	    this.accuracy = 32;
+	  }
+
+	  render(scene, view) {
+	    scene.children.forEach((child) => {
+	      // do some stuff to render the child object
+	      // be sure to check z-buffer
+	      // be sure to check if object is in view [cull]
+	    });
+
+	    return this;
+	  }
+	}
 
 	var webgl = {};
 
@@ -13,7 +29,7 @@
 
 
 	var index = /*#__PURE__*/Object.freeze({
-		Canvas2D: canvas2d,
+		Canvas2D: Canvas2D,
 		WebGL: webgl,
 		WebGL2: webgl2
 	});
@@ -221,8 +237,59 @@
 		Vec3: Vec3
 	});
 
-	exports.Renderer = index;
-	exports.Math = index$1;
+	var arcObject = {};
+
+	var component = {};
+
+	var entity = {};
+
+	var system = {};
+
+	var instance = {};
+
+
+
+	var index$2 = /*#__PURE__*/Object.freeze({
+		ArcObject: arcObject,
+		Component: component,
+		Entity: entity,
+		System: system,
+		Instance: instance
+	});
+
+	// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+	// https://github.com/mrdoob/three.js/blob/dev/src/math/Math.js
+
+	/* eslint-disable no-bitwise, no-mixed-operators, prefer-template */
+	const lut = [];
+
+	for (let i = 0; i < 256; i += 1) {
+	  lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
+	}
+
+	function uuid() {
+	  const d0 = Math.random() * 0xffffffff | 0;
+	  const d1 = Math.random() * 0xffffffff | 0;
+	  const d2 = Math.random() * 0xffffffff | 0;
+	  const d3 = Math.random() * 0xffffffff | 0;
+
+	  return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-'
+	    + lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-'
+	    + lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff]
+	    + lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff]
+	    .toUpperCase();
+	}
+
+
+
+	var index$3 = /*#__PURE__*/Object.freeze({
+		uuid: uuid
+	});
+
+	exports.renderers = index;
+	exports.math = index$1;
+	exports.core = index$2;
+	exports.utils = index$3;
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
--- build/arc-core.module.js
+++ build/arc-core.module.js
@@ -1,4 +1,20 @@
-var canvas2d = {};
+class Canvas2D {
+  constructor() {
+    this.elm = document.createElement('canvas');
+    this.ctx = this.element.getContext('2d');
+    this.accuracy = 32;
+  }
+
+  render(scene, view) {
+    scene.children.forEach((child) => {
+      // do some stuff to render the child object
+      // be sure to check z-buffer
+      // be sure to check if object is in view [cull]
+    });
+
+    return this;
+  }
+}
 
 var webgl = {};
 
@@ -7,7 +23,7 @@ var webgl2 = {};
 
 
 var index = /*#__PURE__*/Object.freeze({
-	Canvas2D: canvas2d,
+	Canvas2D: Canvas2D,
 	WebGL: webgl,
 	WebGL2: webgl2
 });
@@ -215,4 +231,53 @@ var index$1 = /*#__PURE__*/Object.freeze({
 	Vec3: Vec3
 });
 
-export { index as Renderer, index$1 as Math };
+var arcObject = {};
+
+var component = {};
+
+var entity = {};
+
+var system = {};
+
+var instance = {};
+
+
+
+var index$2 = /*#__PURE__*/Object.freeze({
+	ArcObject: arcObject,
+	Component: component,
+	Entity: entity,
+	System: system,
+	Instance: instance
+});
+
+// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+// https://github.com/mrdoob/three.js/blob/dev/src/math/Math.js
+
+/* eslint-disable no-bitwise, no-mixed-operators, prefer-template */
+const lut = [];
+
+for (let i = 0; i < 256; i += 1) {
+  lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
+}
+
+function uuid() {
+  const d0 = Math.random() * 0xffffffff | 0;
+  const d1 = Math.random() * 0xffffffff | 0;
+  const d2 = Math.random() * 0xffffffff | 0;
+  const d3 = Math.random() * 0xffffffff | 0;
+
+  return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-'
+    + lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-'
+    + lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff]
+    + lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff]
+    .toUpperCase();
+}
+
+
+
+var index$3 = /*#__PURE__*/Object.freeze({
+	uuid: uuid
+});
+
+export { index as renderers, index$1 as math, index$2 as core, index$3 as utils };
