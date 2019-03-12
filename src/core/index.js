4176277412ee2428bc3f1df200b7521eaecb9007
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-11 20:47:11 -0400

    index hotfix for core objects

--- src/core/index.js
+++ src/core/index.js
@@ -2,4 +2,4 @@ export { default as ArcObject } from './arc-object';
 export { default as Component } from './component';
 export { default as Entity } from './entity';
 export { default as System } from './system';
-export { defualt as Instance } from './instance';
+export { default as Instance } from './instance';
