96bb6df17f39ce4c1143c5578cb045f7f78fbb55
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-03-11 20:48:56 -0400

    templated render method for canvas 2d

--- src/renderers/canvas-2d/canvas-2d.js
+++ src/renderers/canvas-2d/canvas-2d.js
@@ -1,1 +1,19 @@
-export default {};
+class Canvas2D {
+  constructor() {
+    this.elm = document.createElement('canvas');
+    this.ctx = this.element.getContext('2d');
+    this.accuracy = 32;
+  }
+
+  render(scene, view) {
+    scene.children.forEach((child) => {
+      // do some stuff to render the child object
+      // be sure to check z-buffer
+      // be sure to check if object is in view [cull]
+    });
+
+    return this;
+  }
+}
+
+export default Canvas2D;
