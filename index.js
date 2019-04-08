66005545b83cbdda6d6b2dd4d536095e63a02a4c
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-04-08 19:27:06 -0400

    removed examples

--- index.js
+++
@@ -1,8 +0,0 @@
-const ArcCore = require('./build/arc-core');
-console.log( ArcCore);
-
-const myInstance = new ArcCore.core.Instance();
-
-myInstance.start();
-
-setTimeout(() => { myInstance.stop() }, 1000);
