5a8b379da29e5ef993b143673b2bb30781d5b2c4
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-04-08 20:11:01 -0400

    added comments and corrected code style

--- src/core/arc-object.ts
+++ src/core/arc-object.ts
@@ -1,6 +1,7 @@
+// dependencies
 import { uuid } from '../utils/uuid';
 
 export class ArcObject {
   public readonly uuid: string = uuid()
-  public readonly isArcObject: boolean = true;
+  public readonly isArcObject: boolean = true
 }
