2e28f8ccb86047b45056e1fa2e3fb832d6f402f0
InteractiveTimmy <interactivetimmy@gmail.com>
2019-06-23 13:51:22 -0400

    updated dependencies and scripts, added rollup debug output

--- rollup.config.js
+++ rollup.config.js
@@ -8,9 +8,18 @@ export default {
 			indent: '\t'
 		},
 		{
+			format: 'umd',
+			name: 'ArcCore',
+			file: 'debug/arc-core.js',
+			indent: '\t'
+		},
+		{
 			format: 'es',
 			file: 'build/arc-core.module.js',
 			indent: '\t'
 		}
-	]
+	],
+	watch: {
+		include: "./dist/**/*.js"
+	}
 };
